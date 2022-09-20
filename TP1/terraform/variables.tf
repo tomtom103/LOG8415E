de1c4bdd6ffdd5f0d64e2da208f7454555b1867a
Thomas Caron <thomas.caron@polymtl.ca>
2022-09-20 10:40:48 -0400

    Added more aws variables to terraform

--- TP1/terraform/variables.tf
+++ TP1/terraform/variables.tf
@@ -1,5 +1,25 @@
-variable "number_of_instances" {  
-    # TODO: Change this
-    description = "Number of instances to create"
-    default     = 2
+// AWS specific variables
+variable "aws_access_key" {
+    default = ""
+    description = "AWS access key (AWS_ACCESS_KEY_ID)"
+}
+
+variable "aws_secret_key" {
+    default = ""
+    description = "AWS secret key (AWS_SECRET_ACCESS_KEY)"
+}
+
+variable "aws_region" {
+    description = "AWS region"
+    default = "us-east-1"
+}
+
+variable "app_name" {
+    description = "Name of the application"
+    default = "TP1"
+}
+
+variable "api_version" {
+    description = "Version of the API"
+    default = "latest"
 }

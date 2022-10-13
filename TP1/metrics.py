efd2176d433816c11ea5c21cdd7da04dff30ada4
Slimane <slimaneb2000@gmail.com>
2022-10-13 16:04:39 -0400

    graphs

--- TP1/metrics.py
+++ TP1/metrics.py
@@ -3,17 +3,14 @@ import matplotlib.pyplot as plt
 from datetime import date, timedelta, datetime
 cloudwatch_client = boto3.client('cloudwatch')
 
-yesterday=date.today() - timedelta(days=1)
-tomorrow=date.today() + timedelta(days=1)
 
 #TO CHANGE FOR EACH NEW SESSION
 elb_name = "app/elb/b57c92b6ef0de49a"
-target_group_m4 = "targetgroup/m4/67ce59432e327420"
-target_group_t2 = "targetgroup/t2/2d298dcfdab6976d"
+target_group_m4 = "targetgroup/m4/b071354a3aa6b304"
+target_group_t2 = "targetgroup/t2/98b30930c87eb97e"
 
 #TIME
-st = datetime(yesterday.year, yesterday.month, yesterday.day)
-et = datetime(tomorrow.year, tomorrow.month, tomorrow.day)
+
     
 def get_metric(tg,metric_name,stat) :
     response = cloudwatch_client.get_metric_data(
@@ -40,23 +37,29 @@ def get_metric(tg,metric_name,stat) :
                     }
                 },
             ],
-            StartTime=st, 
-            EndTime=et,    
+            StartTime= datetime.utcnow()- timedelta(days=1), 
+            EndTime= datetime.utcnow()+timedelta(days=1),    
         )
+
+  
     return response['MetricDataResults'][0]['Values']
 
 
 
-# x = [sum(get_metric(target_group_m4,'RequestCount','Sum')),sum(get_metric(target_group_t2,'RequestCount','Sum'))]
-# plt.bar(['m4','t2'],x)
-# plt.title('Number of requests per target group')
-# plt.show()
+x = [sum(get_metric(target_group_m4,'RequestCount','Sum')),sum(get_metric(target_group_t2,'RequestCount','Sum'))]
+plt.bar(['m4','t2'],x)
+plt.title('Number of requests per target group')
+plt.show()
 
 m4 = get_metric(target_group_m4,'TargetResponseTime','Average')
 t2 = get_metric(target_group_t2,'TargetResponseTime','Average')
 x = [sum(m4)/len(m4), sum(t2)/len(t2)]
 plt.bar(['m4','t2'],x)
-plt.title(' average response time per target group')
+plt.title('Average response time per target group')
 plt.show()
+
+# m4 = get_metric(target_group_m4,'RequestCountPerTarget','Sum')
+# t2 = get_metric(target_group_t2,'RequestCountPerTarget','Sum')
+
 
 

02aabf05a9a7fa72a458cd73cfe2eba04f700568
Kevin Lam <kevinlam@Kevins-MacBook-Pro.local>
2022-10-13 13:18:02 -0400

    first version with thread

--- TP1/request.py
+++ TP1/request.py
@@ -2,12 +2,6 @@ import requests
 import time
 from threading import Thread
 
-def call_endpoint_http():
-    url = "http://alb-test-1171701604.us-east-1.elb.amazonaws.com"
-    headers = {'content-type': 'application/json'}
-    r = requests.get(url, headers=headers)
-    print(r.status_code)
-
 def run_thread1_requests(url):
     for i in range(1000):
         r = requests.get(url)
@@ -20,3 +14,23 @@ def run_thread2_requests(url):
     do_500_request(url)
     time.sleep(60)
     run_thread1_requests(url)
+
+def start_threads(threads):
+    for thread in threads:
+        thread.start()
+
+def join_threads(threads):
+    for thread in threads:
+        thread.join()
+
+if __name__ == "__main__":
+    # should get this info in terraform or directly from the ui ? 
+    url = ""
+
+    # need to add /cluster1 and /cluster2 for the good thread
+    first_thread = Thread(target=run_thread1_requests, args=(url))
+    second_thread = Thread(target=run_thread2_requests, args=(url))
+
+    start_threads([first_thread, second_thread])
+
+    join_threads([first_thread, second_thread])

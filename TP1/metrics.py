29b198775c2da38ec610532e97bc2db9dcf21571
Slimane <slimaneb2000@gmail.com>
2022-10-13 11:00:20 -0400

    added avg response time

--- TP1/metrics.py
+++ TP1/metrics.py
@@ -6,13 +6,16 @@ cloudwatch_client = boto3.client('cloudwatch')
 yesterday=date.today() - timedelta(days=1)
 tomorrow=date.today() + timedelta(days=1)
 
+#TO CHANGE FOR EACH NEW SESSION
 elb_name = "app/elb/b57c92b6ef0de49a"
 target_group_m4 = "targetgroup/m4/67ce59432e327420"
 target_group_t2 = "targetgroup/t2/2d298dcfdab6976d"
+
+#TIME
 st = datetime(yesterday.year, yesterday.month, yesterday.day)
 et = datetime(tomorrow.year, tomorrow.month, tomorrow.day)
     
-def get_request_count(tg) :
+def get_metric(tg,metric_name,stat) :
     response = cloudwatch_client.get_metric_data(
             MetricDataQueries=[
                 {
@@ -20,7 +23,7 @@ def get_request_count(tg) :
                     'MetricStat': {
                         'Metric': {
                             'Namespace': 'AWS/ApplicationELB',
-                            'MetricName': 'RequestCount',
+                            'MetricName': metric_name,
                             'Dimensions': [
                                 {
                                     'Name': 'TargetGroup',
@@ -32,24 +35,28 @@ def get_request_count(tg) :
                                 },
                             ]
                         },
-                        'Period': 300,
-                        'Stat': 'Sum',
-                        'Unit': 'Count'
+                        'Period': 300, #5 min period
+                        'Stat': stat,
                     }
                 },
             ],
             StartTime=st, 
             EndTime=et,    
         )
-    return sum(response['MetricDataResults'][0]['Values'])
+    return response['MetricDataResults'][0]['Values']
 
 
 
-#print(get_request_count(target_group_m4))
-#print(get_request_count(target_group_t2))
+# x = [sum(get_metric(target_group_m4,'RequestCount','Sum')),sum(get_metric(target_group_t2,'RequestCount','Sum'))]
+# plt.bar(['m4','t2'],x)
+# plt.title('Number of requests per target group')
+# plt.show()
 
-x = [get_request_count(target_group_m4),get_request_count(target_group_t2)]
+m4 = get_metric(target_group_m4,'TargetResponseTime','Average')
+t2 = get_metric(target_group_t2,'TargetResponseTime','Average')
+x = [sum(m4)/len(m4), sum(t2)/len(t2)]
 plt.bar(['m4','t2'],x)
-plt.title('Number of requests per target group')
+plt.title(' average response time per target group')
 plt.show()
+
 

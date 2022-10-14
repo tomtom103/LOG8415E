aeecfea3d286b511d9e9c616972663e392856aba
Kevin Lam <kevinlam@Kevins-MacBook-Pro.local>
2022-10-14 16:28:29 -0400

    region name for client

--- TP1/request.py
+++ TP1/request.py
@@ -26,7 +26,7 @@ def join_threads(threads):
 
 if __name__ == "__main__":
 
-    client = boto3.client('elbv2')
+    client = boto3.client('elbv2', region_name='us-east-1')
     url = client.describe_load_balancers()['LoadBalancers'][0]['DNSName']
 
     first_thread = Thread(target=run_thread1_requests, args=(url + "/cluster1"))

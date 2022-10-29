28b39db6ca34531739a679ec72438eae2fa33383
Kevin Lam <kevinlam@Kevins-MacBook-Pro.local>
2022-10-29 15:05:20 -0400

    start of the emit value class

---
+++ TP2/FriendPairWritable.java
@@ -0,0 +1,11 @@
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import org.apache.hadoop.io.Writable;
+
+
+// This class need to be created since the emitted value 
+// is not a primitive data type in hadoop.
+public class FriendPairWritable implements Writable {
+    
+}   

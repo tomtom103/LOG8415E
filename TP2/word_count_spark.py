bef3fb49b9cc7e4b56bbc0e9855436c853e22dc1
Slimane <slimaneb2000@gmail.com>
2022-10-29 13:26:21 -0400

    getting execution time for spark

--- TP2/word_count_spark.py
+++ TP2/word_count_spark.py
@@ -1,5 +1,10 @@
 from pyspark.sql import SparkSession
 import sys
+import time
+
+
+
+time_f = open("./files/time.txt", "a")
 
 #start a spark session
 session= SparkSession.builder.getOrCreate()
@@ -10,15 +15,24 @@ spark = session.sparkContext
 #read the file from the path sent from the command line
 file = spark.textFile(sys.argv[1])
 
+start_time = time.time()
+
 #Map : emitting each word with a value of one
 wordList = file.flatMap(lambda l: l.split(" ")).map(lambda w : (w,1))
 
 #Reduce : summing the count for each word
-count = wordList.reduceByKey(lambda w1,w2: w1+w2).collect()
+count = wordList.reduceByKey(lambda c1,c2: c1+c2)
+
+end_time = time.time() - start_time
+
 
 #print 
-for w in count :
+result = count.collect()
+for w in result :
     print(w)
+
+time_f.write(str(end_time) + '\n')
+time_f.close()
 
 
 

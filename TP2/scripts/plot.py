08ee723cda78ce7ef25885601aa703fde2694cde
Slimane <slimaneb2000@gmail.com>
2022-10-30 16:02:30 -0400

    refactoring

--- TP2/scripts/plot.py
+++ TP2/scripts/plot.py
@@ -1,24 +1,30 @@
 import matplotlib.pyplot as plt
 
-f = open('../files/time.txt', 'r')
-lines = f.readlines()
-f.close()
+def plot_data_from_file(path, app_name) :
+    f = open(path, 'r')
+    lines = f.readlines()
+    f.close()
 
-data = {}
+    data = {}
 
-for l in lines :
-    file,time = l.split(' ')
-    file = file.split(':')[1]
-    time = float(time.split(':')[1].rstrip())
-    if file not in data.keys() :
-        data[file] = []
-    data[file].append(time)
+    #reading from the data file and putting the relevant info in data structure
+    for l in lines :
+        file,time = l.split(' ')
+        file = file.split(':')[1]
+        time = float(time.split(':')[1].rstrip())
+        if file not in data.keys() :
+            data[file] = []
+        data[file].append(time)
 
-data = dict(map(lambda kv : (kv[0],sum(kv[1])/len(kv[1])), data.items()))
+    #Reorganizing the data structure to get the average time for each file
+    data = dict(map(lambda kv : (kv[0],sum(kv[1])/len(kv[1])), data.items()))
 
-plt.rcParams["figure.figsize"] = (13,5)
-fig, ax = plt.subplots()
-bars = ax.bar(list(data.keys()),list(data.values()))
-ax.bar_label(bars)
-plt.title('Average time of the spark wordcount on each input file')
-plt.savefig('../metrics/spark_avg_time.png')
+    #plotting a bar chart
+    plt.rcParams["figure.figsize"] = (13,5)
+    fig, ax = plt.subplots()
+    bars = ax.bar(list(data.keys()),list(data.values()))
+    ax.bar_label(bars)
+    plt.title('Average time of the ' + app_name + ' wordcount on each input file')
+    plt.savefig('../metrics/'+app_name+'_avg_time.png')
+
+plot_data_from_file('../files/time.txt', 'spark')

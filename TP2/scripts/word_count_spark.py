6f852e8741003c337e78c841a4712d26eb067093
Slimane <slimaneb2000@gmail.com>
2022-10-30 14:18:29 -0400

    generated the output files for spark

--- TP2/scripts/word_count_spark.py
+++ TP2/scripts/word_count_spark.py
@@ -7,15 +7,15 @@ input_file_name = path.split('/')[-1]
 time_f = open("../files/time.txt", "a")
 
 #start a spark session
-session= SparkSession.builder.getOrCreate()
+session = SparkSession.builder.getOrCreate()
 
 #Initialize the context
 spark = session.sparkContext
 
+start_time = time.time()
+
 #read the file from the path sent from the command line
 file = spark.textFile(path)
-
-start_time = time.time()
 
 #Map : emitting each word with a value of one
 wordList = file.flatMap(lambda l: l.split(" ")).map(lambda w : (w,1))
@@ -25,13 +25,17 @@ count = wordList.reduceByKey(lambda c1,c2: c1+c2)
 
 end_time = time.time() - start_time
 
-
-#print all the words with their count
+#print the words with their count in output file
+output_file = open('../files/output/' + input_file_name.split('.')[0] + '_output_spark.txt', 'w')
 result = count.collect()
 for w in result :
-    print(w)
+    output_file.write(str(w) + '\n')
+
 #writing the map reduce execution time in data file
 time_f.write('Filename:' + input_file_name + " time:" + str(end_time) + '\n')
+
+print(end_time)
+output_file.close()
 time_f.close()
 
 

779e9f4ea4d129653d10db4c2de1b93e22a01f6e
GhaziBna <ghazi.ben-achour@polymtl.ca>
2022-10-31 14:01:28 -0400

    Adding comments

--- TP2/RecommendationMR/Recommendation.java
+++ TP2/RecommendationMR/Recommendation.java
@@ -28,29 +28,29 @@ public class Recommendation {
         public void map(LongWritable key, Text value, Context context)
                 throws IOException, InterruptedException {
             
-            // Splitting line between userId and friend list (split[0] is the userId and split[1] is his friends list)
+            // Split line between userId and friend list (line[0] is the userId and line[1] is his friends list)
             String[] line = value.toString().split("\\t");
             String userId = line[0];
             this.userId.set(userId);
             
-            // Emit the current friends of the user with an indicator or when the user doesn't have a friend
-            // The indicator /EN 
+            // Emit first degree friends of the user with an indicator at the end of the line (/DEG1)
             if (line.length == 1) {
-                // This case handles when the user doesn't have a friend
-                friendsList.set("/CURRENT");
+                // This case handles the case when the user doesn't have a friend
+                friendsList.set("/DEG1");
                 context.write(this.userId, this.friendsList);
                 return;
             } else {
-                friendsList.set(line[1] + "/CURRENT");
+                friendsList.set(line[1] + "/DEG1");
                 context.write(this.userId, this.friendsList);
             }
 
+            // Split friends list (separated by a comma) in a string array
             String[] friendsIdList = line[1].split(",");
             
-            // for each friends, emit potential mutual friend
+            // For each friends, emit second degree friends with an indicator at the end (/DEG2)
             for (String friend : friendsIdList) {
                 String friendsInCommon = Arrays.stream(friendsIdList).filter(compFr -> !compFr.equals(friend)).collect(Collectors.joining(","));
-                this.friendsList.set(friendsInCommon + "/POTMUTFRIEND");
+                this.friendsList.set(friendsInCommon + "/DEG2");
                 this.userId.set(friend);
                 context.write(this.userId, this.friendsList);
             }
@@ -70,11 +70,14 @@ public class Recommendation {
 
                 // Populate dict (key is userId, value are the mutual friend they have with them)
                 for (Text value : values) {
+                    // Split input value between friends list and degree indicator
                     String[] line = value.toString().split("/");
+                    
+                    // Split each friend in the list in string array
                     String[] friends = line[0].split(",");
 
                     if(friends.length > 1 || !friends[0].equals("")){
-                        if (line[1].equals("CURRENT")) {
+                        if (line[1].equals("DEG1")) {
                             currentFriends = friends;
                         } else {
                             for (String friend : friends) {
@@ -86,7 +89,7 @@ public class Recommendation {
                     }
                 }
                 
-                // Remove own friends for the user
+                // Remove first degree friends
                 recommendations.remove("");
                 for (String friend : currentFriends) {
                     recommendations.remove(friend);
@@ -98,9 +101,10 @@ public class Recommendation {
 
     /*
     This function trim the list of recommendations to get only the top 10 recommendations and sort
-    the list in a decreasing order.AbstractMap
+    the list in a decreasing order
      */
     public static String getTop10Recommendations(HashMap<String, Integer> recommendations) {
+        // comparing number of mutual friends, then user's key
         Comparator<Map.Entry<String, Integer>> sortLogic = ((a, b) -> b.getValue().compareTo(a.getValue()));
         sortLogic = sortLogic.thenComparing(a -> a.getKey());
         return recommendations.entrySet()
@@ -121,16 +125,18 @@ public class Recommendation {
         }
 
         Job recJob = Job.getInstance(config, "MapReduce Friends Recommendation");
+        
+        // Set jar, mapper and reducer classes
         recJob.setJarByClass(Recommendation.class);
         recJob.setMapperClass(RecommendationMapper.class);
         recJob.setReducerClass(RecommendationReducer.class);
+        
+        // Set output key and value type
         recJob.setOutputKeyClass(Text.class);
         recJob.setOutputValueClass(Text.class);
 
-        for(int i = 0; i< pathArgs.length-1; ++i) {
-            FileInputFormat.addInputPath(recJob, new Path(pathArgs[i]));
-        }
-
+        // Set HDFS input and output paths
+        FileInputFormat.addInputPath(recJob, new Path(pathArgs[0]));
         FileOutputFormat.setOutputPath(recJob, new Path(pathArgs[pathArgs.length-1]));
 
         System.exit(recJob.waitForCompletion(true) ? 0 : 1);
